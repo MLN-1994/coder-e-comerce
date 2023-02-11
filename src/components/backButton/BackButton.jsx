410bc2a6726ca8cecc48eef6ab0c9679621c00b1
Mariano <marianolangge@gmail.com>
2023-02-11 00:24:01 -0300

    bckbuttonResponsive

--- src/components/backButton/BackButton.jsx
+++ src/components/backButton/BackButton.jsx
@@ -7,9 +7,9 @@ function BackButton() {
   };
 
   return (
-    <div className="grid grid-cols-12 m-2 ">
+    <div className="grid lg:grid-cols-12  m-2 ">
         <button
-          className="p-2  bg-blue-400 hover:bg-blue-500 col-start-10 w-auto text-white rounded-md"
+          className="p-2  bg-blue-400 hover:bg-blue-500 col-start-10 w-auto  text-white rounded-md"
           onClick={handleBack}
         >
           Volver
